84a43a77743aa9e77f53d0c354e3117316c94726
defitas <syarifdefita@gmail.com>
2026-06-02 09:46:32 +0700

    ninth commit

--- db/lms.sql
+++ db/lms.sql
@@ -7,7 +7,7 @@
 -- Server version: 10.1.38-MariaDB
 -- PHP Version: 7.3.3
 
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET SQL_MODE = "";
 SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
